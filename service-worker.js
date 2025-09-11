f65ee704f254c734b05b0cac6dc17cec7b1c4a2a
matahari999 <163366505+matahari999@users.noreply.github.com>
2025-09-11 09:21:44 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+<script>
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(reg => console.log('✅ Service Worker 등록 성공:', reg.scope))
+        .catch(err => console.error('❌ Service Worker 등록 실패:', err));
+    });
+  }
+</script>
